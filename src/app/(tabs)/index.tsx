0e0992134bafd2304d2fe53fffd264ee64463be0
Ritesh Pal <ritesh.dm.211@gmail.com>
2026-06-15 14:48:51 +0530

    feat: implement global theme context

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { useTheme } from '../../core/theme/ThemeContext';
 
 export default function Home() {
+  const { theme, toggleTheme } = useTheme();
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to LedgerLite!</Text>
+      <Text>Current Theme: {theme}</Text>
+      <Button title='Toggle Theme' onPress={toggleTheme} />
     </View>
   );
 }
