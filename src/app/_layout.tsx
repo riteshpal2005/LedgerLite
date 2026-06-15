fcb22ec5edd78badba5ec4b8fcbf63c907517d38
Ritesh Pal <ritesh.dm.211@gmail.com>
2026-06-15 17:26:04 +0530

    feat: implement SQLiteProvider and schema migrations

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack } from "expo-router";
 import { ThemeProvider } from "../core/theme/ThemeContext";
 import '../global.css';
+import { SQLiteProvider } from "expo-sqlite";
+import { initializeDatabase } from "../core/database/schema";
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <Stack>
-        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
+    <SQLiteProvider databaseName="ledgerlite.db" onInit={initializeDatabase}>
+      <ThemeProvider>
+        <Stack>
+          <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    </SQLiteProvider>
   );
 }
---
+++ src/core/database/schema.ts
@@ -0,0 +1,45 @@
+import { SQLiteDatabase } from 'expo-sqlite';
+
+
+export type TransactionType = 'credit' | 'debit';
+
+export interface Expense {
+  id: number;
+  amount: number;
+  description: string;
+  date: number;
+  type: TransactionType;
+  categoryId: number;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  icon: string;
+  color: string;
+}
+
+export const CREATE_CATEGORIES_TABLE = `
+  CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    icon TEXT,
+    color TEXT
+  );
+`;
+
+export const CREATE_EXPENSES_TABLE = `
+  CREATE TABLE IF NOT EXISTS expenses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    amount REAL NOT NULL,
+    description TEXT,
+    date INTEGER NOT NULL,
+    categoryId INTEGER NOT NULL,
+    type TEXT NOT NULL
+  );
+`;
+
+export async function initializeDatabase(db: SQLiteDatabase) {
+  await db.execAsync(CREATE_CATEGORIES_TABLE);
+  await db.execAsync(CREATE_EXPENSES_TABLE);
+}
