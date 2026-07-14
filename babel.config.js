f49759d9196de89d575cceb290094c32fe3dbd12
Ritesh Pal <ritesh.dm.211@gmail.com>
2026-07-15 04:55:30 +0530

    build: install watermelondb and configure babel decorators

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,7 @@ module.exports = function (api) {
       "nativewind/babel",
     ],
     plugins: [
+      ["@babel/plugin-proposal-decorators", { "legacy": true }],
       'react-native-reanimated/plugin',
     ],
   };
