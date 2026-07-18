35f59462be8e61474c832a047b2552b20e20f6bd
Ritesh Pal <ritesh.dm.211@gmail.com>
2026-07-18 19:32:50 +0530

    feat: add useCurrency hook

---
+++ src/hooks/useCurrency.ts
@@ -0,0 +1,31 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+import { 
+  CurrencyCode, 
+  CurrencyConfig, 
+  MAJOR_CURRENCIES, 
+  formatCurrency as baseFormatCurrency, 
+  getCurrencySymbol as baseGetCurrencySymbol 
+} from '../utils/currency';
+
+export function useCurrency() {
+  const currencyCode = useSelector((state: RootState) => state.settings.currency) as CurrencyCode;
+  
+  // Fallback to INR if somehow the state has an invalid currency
+  const config: CurrencyConfig = MAJOR_CURRENCIES[currencyCode] || MAJOR_CURRENCIES['INR'];
+
+  const formatCurrency = (amount: number) => {
+    return baseFormatCurrency(amount, config.code);
+  };
+
+  const getCurrencySymbol = () => {
+    return baseGetCurrencySymbol(config.code);
+  };
+
+  return {
+    currencyCode: config.code,
+    currencyConfig: config,
+    formatCurrency,
+    getCurrencySymbol,
+  };
+}
